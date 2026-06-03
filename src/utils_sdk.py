bdd263c4cf638353a45fae236d92230666a58ab4
Peter Kral <peter.kral@pdfix.net>
2026-06-03 17:29:28 +0200

    fix(PVQ-5466): Fixes for None (added checks and Optional typehints).

--- src/utils_sdk.py
+++ src/utils_sdk.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pdfixsdk import Pdfix
+from pdfixsdk import Pdfix, PsAccountAuthorization, PsStandardAuthorization
 
 from exceptions import PdfixActivationException, PdfixAuthorizationException
 
@@ -15,11 +15,12 @@ def authorize_sdk(pdfix: Pdfix, license_name: Optional[str], license_key: Option
         license_key (string): Pdfix sdk license key
     """
     if license_name and license_key:
-        authorization = pdfix.GetAccountAuthorization()
-        if not authorization.Authorize(license_name, license_key):
+        authorization: Optional[PsAccountAuthorization] = pdfix.GetAccountAuthorization()
+        if authorization is None or not authorization.Authorize(license_name, license_key):
             raise PdfixAuthorizationException(pdfix)
     elif license_key:
-        if not pdfix.GetStandardAuthorization().Activate(license_key):
+        standard_authorization: Optional[PsStandardAuthorization] = pdfix.GetStandardAuthorization()
+        if standard_authorization is None or not standard_authorization.Activate(license_key):
             raise PdfixActivationException(pdfix)
     else:
         print("No license name or key provided. Using PDFix SDK trial")
